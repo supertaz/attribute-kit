76aeccd68862a5e48a279705097c2d0a565b66df
Jon Mischo <jon.mischo@gmail.com>
2011-07-15 19:30:11 -0700

    This library is designed for 1.9, but a 1.8ism crept in - instance_methods returns an array of symbols, not strings!

--- lib/attribute-kit/attribute_hash.rb
+++ lib/attribute-kit/attribute_hash.rb
@@ -235,7 +235,7 @@ module AttributeKit
         when /(.*)_deleted?/
           @deleted_keys.include?($1) || @deleted_keys.include?($1.to_sym)
         else
-          if self.class.superclass.instance_methods.include?('method_missing')
+          if self.class.superclass.instance_methods.include?(:method_mising)
             super(method, *args, &block)
           else
             raise NoMethodError.new("undefined method '#{method_name}' for #{self}")
